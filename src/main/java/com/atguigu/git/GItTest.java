66d5fc5a4643269a84938b5f010d70b443865d84
yao <yao@luanji>
2023-03-08 12:45:44 +0800

    hot1 commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -18,6 +18,7 @@ public class GItTest {
         System.out.println("gitee test2!");
         System.out.println("gitee test3!");
         System.out.println("gitee test4!");
+        System.out.println("hotfix111");
 
     }
 }
